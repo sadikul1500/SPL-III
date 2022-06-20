7a5b129c157d2b05efec4fced7c5f332a261135e
CI-CD <bsse1003@iit.du.ac.bd>
2022-06-21 01:04:56 +0600

    association video class written... not tested

--- lib/Lessons/Association/association_video.dart
+++ lib/Lessons/Association/association_video.dart
@@ -29,52 +29,48 @@ class AssociationVideoCard {
   double bufferingProgress = 0.0;
   Media? metasMedia;
   List<File> files = [];
-  //String _selectedFiles = '';
+
+  AssociationCard(String videoFilePath) {
+    player.currentStream.listen((current) {
+      this.current = current;
+    });
+    player.positionStream.listen((position) {
+      this.position = position;
+    });
+    player.playbackStream.listen((playback) {
+      this.playback = playback;
+    });
+    player.generalStream.listen((general) {
+      general = general;
+    });
+    player.videoDimensionsStream.listen((videoDimensions) {
+      videoDimensions = videoDimensions;
+    });
+    player.bufferingProgressStream.listen(
+      (bufferingProgress) {
+        bufferingProgress = bufferingProgress;
+      },
+    );
+    player.errorStream.listen((event) {
+      throw Error(); //'libvlc error.'
+    });
+    devices = Devices.all;
+    Equalizer equalizer = Equalizer.createMode(EqualizerMode.live);
+    equalizer.setPreAmp(10.0);
+    equalizer.setBandAmp(31.25, 10.0);
+    player.setEqualizer(equalizer);
+    player.open(Media.file(File(videoFilePath)), autoStart: false);
+  }
 
   //@override
-  // void initState() {
-  //   super.initState();
-  //   if (mounted) {
-  //     player.currentStream.listen((current) {
-  //       setState(() => this.current = current);
-  //     });
-  //     player.positionStream.listen((position) {
-  //       setState(() => this.position = position);
-  //     });
-  //     player.playbackStream.listen((playback) {
-  //       setState(() => this.playback = playback);
-  //     });
-  //     player.generalStream.listen((general) {
-  //       setState(() => general = general);
-  //     });
-  //     player.videoDimensionsStream.listen((videoDimensions) {
-  //       setState(() => videoDimensions = videoDimensions);
-  //     });
-  //     player.bufferingProgressStream.listen(
-  //       (bufferingProgress) {
-  //         setState(() => bufferingProgress = bufferingProgress);
-  //       },
-  //     );
-  //     player.errorStream.listen((event) {
-  //       print('libvlc error.');
-  //     });
-  //     devices = Devices.all;
-  //     Equalizer equalizer = Equalizer.createMode(EqualizerMode.live);
-  //     equalizer.setPreAmp(10.0);
-  //     equalizer.setBandAmp(31.25, 10.0);
-  //     player.setEqualizer(equalizer);
-  //   }
-  // }
-
-  //@override
-  Widget associationVideoCard(BuildContext context) {
+  Widget associationVideoCard() {
     return SizedBox(
       height: 420,
       width: 600,
       child: NativeVideo(
         player: player,
         width: 600, //640,
-        height: 420,//360,
+        height: 420, //360,
         volumeThumbColor: Colors.blue,
         volumeActiveColor: Colors.blue,
         showControls: true, //!isPhone
