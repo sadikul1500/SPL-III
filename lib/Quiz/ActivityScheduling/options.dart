3ab09b8d17dcf23d9592ddca4a22d4e67a92f966
CI-CD <bsse1003@iit.du.ac.bd>
2022-07-06 01:11:32 +0600

    started to build.... need to continue consistently...

---
+++ lib/Quiz/ActivityScheduling/options.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class ActivityOptions extends StatefulWidget {
+  @override
+  State<ActivityOptions> createState() => _ActivityOptionsState();
+}
+
+class _ActivityOptionsState extends State<ActivityOptions> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Activity schedulinf test'),
+          centerTitle: true,
+        ),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            ElevatedButton(
+                onPressed: () {
+                  //code goes here
+                },
+                child: const Text('Choose from saved items')),
+            ElevatedButton(
+                onPressed: () {
+                  //code goes here
+                },
+                child: const Text('Create a new question'))
+          ],
+        )));
+  }
+}
