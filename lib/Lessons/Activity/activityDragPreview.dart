54b2286676634b36cd40dc4bfd20a8015f5b020d
CI-CD <bsse1003@iit.du.ac.bd>
2022-07-14 08:35:31 +0600

    need to test drag&drop

--- lib/Lessons/Activity/activityDragPreview.dart
+++ lib/Lessons/Activity/activityDragPreview.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 class ItemModel {
@@ -9,6 +11,7 @@ class ItemModel {
 
 class ActivityDrag extends StatefulWidget {
   final List<ItemModel> items;
+  //List<ItemModel> values = [];
   //final List<ItemModel> items2;
   final String question;
   const ActivityDrag(this.items, this.question); //this.items2,
@@ -17,15 +20,19 @@ class ActivityDrag extends StatefulWidget {
 }
 
 class _ActivityDragState extends State<ActivityDrag> {
+  List<ItemModel> values = [];
   @override
   initState() {
     widget.items.shuffle();
+    //var values = List<int>.generate(widget.items.length, (i) => i);
+    for (int i = 0; i < widget.items.length; i++) {
+      values.add(ItemModel(i.toString()));
+    }
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return SingleChildScrollView(
         child: Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -33,18 +40,117 @@ class _ActivityDragState extends State<ActivityDrag> {
         Container(
           padding: const EdgeInsets.all(5),
           height: MediaQuery.of(context).size.height,
-          width: 400,
+          width: 350,
           decoration:
               BoxDecoration(border: Border.all(color: Colors.black, width: 2)),
-          child: Wrap(),
+          child: Wrap(
+            direction: Axis.horizontal,
+            spacing: 10,
+            runSpacing: 10,
+            children: widget.items.map((item) {
+              return Draggable<ItemModel>(
+                data: item,
+                childWhenDragging: SizedBox(
+                    height: 150,
+                    width: 150,
+                    child: Image.file(
+                      File(item.value.split(' ').first),
+                      fit: BoxFit.contain,
+                      filterQuality: FilterQuality.high,
+                      //colorBlendMode: BlendMode.darken,
+                    )),
+                feedback: SizedBox(
+                    height: 150,
+                    width: 150,
+                    child: Image.file(
+                      File(item.value.split(' ').first),
+                      fit: BoxFit.contain,
+                      filterQuality: FilterQuality.high,
+                    )),
+                child: SizedBox(
+                    height: 150,
+                    width: 150,
+                    child: Image.file(
+                      File(item.value.split(' ').first),
+                      fit: BoxFit.contain,
+                      filterQuality: FilterQuality.high,
+                    )),
+              );
+            }).toList(),
+          ),
         ),
         Container(
           padding: const EdgeInsets.all(5),
           height: MediaQuery.of(context).size.height,
-          width: 400,
+          width: 350,
           decoration:
               BoxDecoration(border: Border.all(color: Colors.black, width: 2)),
-          child: Wrap(),
+          child: Wrap(
+            children: values.map((item) {
+              return Container(
+                margin: const EdgeInsets.fromLTRB(25, 10, 100, 8.0),
+                child: DragTarget<ItemModel>(
+                  onAccept: (receivedItem) async {
+                    if (item.value == receivedItem.value.split(' ').last) {
+                      setState(() {
+                        // playConfetti = true;
+                        // _smallConfettiController.play();
+                        // _confettiRightController.play();
+                        // _confettiLeftController.play();
+
+                        widget.items.remove(receivedItem);
+                        // widget.items2.remove(item);
+                        //dispose();
+                        // score += 1;
+                        item = receivedItem;
+                        item.accepting = false;
+                      });
+
+                      //await audioPlay();
+                    } else {
+                      setState(() {
+                        //score -= 1;
+                        item.accepting = false;
+                        //playConfetti = false;
+                      });
+                    }
+                  },
+                  onLeave: (receivedItem) {
+                    setState(() {
+                      item.accepting = false;
+                      //playConfetti = false;
+                      //audioPlayer.stop();
+                    });
+                  },
+                  onWillAccept: (receivedItem) {
+                    setState(() {
+                      item.accepting = true;
+                      //playConfetti = false;
+                    });
+                    return true;
+                  },
+                  builder: (context, acceptedItem, rejectedItem) => Container(
+                    color: item.accepting ? Colors.red : Colors.blue,
+                    height: 150,
+                    width: 150,
+                    alignment: Alignment.center,
+                    //margin: const EdgeInsets.all(8.0),
+                    child: item.value.length > 4
+                        ? Image.file(
+                            File(item.value.split(' ').first),
+                            fit: BoxFit.contain,
+                            filterQuality: FilterQuality.high,
+                          )
+                        : Text(item.value,
+                            style: const TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 22.0)),
+                  ),
+                ),
+              );
+            }).toList(),
+          ),
         )
       ],
     ));
