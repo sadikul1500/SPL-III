4f5110fcd30d3d57b19ae574d4012292b142dfb0
CI-CD <bsse1003@iit.du.ac.bd>
2022-07-19 01:01:05 +0600

    do activity quiz finishing.......

--- lib/Lessons/Activity/activityDragPreview.dart
+++ lib/Lessons/Activity/activityDragPreview.dart
@@ -33,6 +33,7 @@ class _ActivityDragState extends State<ActivityDrag> {
       items.add(ItemModel(widget.files[i].path + ' space ' + i.toString()));
     }
     //print(items[0].value);
+    items.shuffle();
     super.initState();
   }
 
@@ -44,140 +45,147 @@ class _ActivityDragState extends State<ActivityDrag> {
         centerTitle: true,
       ),
       body: SingleChildScrollView(
+          padding: const EdgeInsets.fromLTRB(0, 15, 0, 15),
           child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          Container(
-            constraints: const BoxConstraints(minHeight: 400),
-            padding: const EdgeInsets.all(5),
-            //height: MediaQuery.of(context).size.height,
-            width: 400,
-            decoration: BoxDecoration(
-                border: Border.all(color: Colors.black, width: 2)),
-            child: Wrap(
-              direction: Axis.horizontal,
-              spacing: 10,
-              runSpacing: 10,
-              children: items.map((item) {
-                return Draggable<ItemModel>(
-                  data: item,
-                  childWhenDragging: Container(
-                      alignment: Alignment.center,
-                      height: 145,
-                      width: 145,
-                      child: Image.file(
-                        File(item.value.split(' space ').first),
-                        fit: BoxFit.contain,
-                        //filterQuality: FilterQuality.high,
-                        colorBlendMode: BlendMode.darken,
-                      )),
-                  feedback: SizedBox(
-                      //child that I drop....
-                      height: 170,
-                      width: 170,
-                      child: Image.file(
-                        File(item.value.split(' space ').first),
-                        fit: BoxFit.contain,
-                        //filterQuality: FilterQuality.high,
-                      )),
-                  child: Container(
-                      height: 150,
-                      width: 150,
-                      alignment: Alignment.center,
-                      child: Image.file(
-                        File(item.value.split(' space ').first),
-                        fit: BoxFit.contain,
-                        filterQuality: FilterQuality.high,
-                      )),
-                );
-              }).toList(),
-            ),
-          ),
-          Container(
-            constraints: const BoxConstraints(minHeight: 400),
-            padding: const EdgeInsets.all(5),
-            //height: MediaQuery.of(context).size.height,
-            width: 400,
-            decoration: BoxDecoration(
-                border: Border.all(color: Colors.black, width: 2)),
-            child: Wrap(
-              direction: Axis.horizontal,
-              spacing: 10,
-              runSpacing: 10,
-              children: values.map((item) {
-                return DragTarget<ItemModel>(
-                  onAccept: (receivedItem) async {
-                    if (item.value ==
-                        receivedItem.value.split(' space ').last) {
-                      setState(() {
-                        // playConfetti = true;
-                        // _smallConfettiController.play();
-                        // _confettiRightController.play();
-                        // _confettiLeftController.play();
-
-                        items.remove(receivedItem);
-                        // widget.items2.remove(item);
-                        //dispose();
-                        // score += 1;
-                        //item = receivedItem;
-                        item.accepting = false;
-                        item.isSuccessful = true;
-                        item.value = receivedItem.value;
-                        print('....matched......'); //hot reload.....
-                      });
-
-                      //await audioPlay();
-                    } else {
-                      setState(() {
-                        //score -= 1;
-                        print('*' +
-                            item.value +
-                            '*aaa*' +
-                            receivedItem.value.split(' space ').last +
-                            "*");
-                        item.accepting = false;
-                        //playConfetti = false;
-                      });
-                    }
-                  },
-                  onLeave: (receivedItem) {
-                    setState(() {
-                      item.accepting = false;
-                      //playConfetti = false;
-                      //audioPlayer.stop();
-                    });
-                  },
-                  onWillAccept: (receivedItem) {
-                    setState(() {
-                      item.accepting = true;
-                      //playConfetti = false;
-                    });
-                    return true;
-                  },
-                  builder: (context, acceptedItem, rejectedItem) => Container(
-                    color: item.accepting ? Colors.red : Colors.blue,
-                    height: 150,
-                    width: 150,
-                    alignment: Alignment.center,
-                    //margin: const EdgeInsets.all(8.0),
-                    child: item.isSuccessful
-                        ? Image.file(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Container(
+                constraints: const BoxConstraints(minHeight: 400),
+                padding: const EdgeInsets.all(10),
+                //height: MediaQuery.of(context).size.height,
+                width: 550,
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black, width: 2)),
+                child: Wrap(
+                  direction: Axis.horizontal,
+                  spacing: 10,
+                  runSpacing: 10,
+                  children: items.map((item) {
+                    return Draggable<ItemModel>(
+                      data: item,
+                      childWhenDragging: Container(
+                          alignment: Alignment.center,
+                          height: 168,
+                          width: 248,
+                          child: Image.file(
+                              File(item.value.split(' space ').first),
+                              fit: BoxFit.contain,
+                              filterQuality: FilterQuality.high,
+                              colorBlendMode: BlendMode.modulate,
+                              color: Colors.white.withOpacity(0.4))),
+                      feedback: SizedBox(
+                          //child that I drop....
+                          height: 168,
+                          width: 248,
+                          child: Image.file(
                             File(item.value.split(' space ').first),
                             fit: BoxFit.contain,
                             filterQuality: FilterQuality.high,
-                          )
-                        : Text((int.parse(item.value) + 1).toString(),
-                            style: const TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 22.0)),
-                  ),
-                );
-              }).toList(),
-            ),
-          )
-        ],
-      )),
+                          )),
+                      child: SizedBox(
+                          height: 168,
+                          width: 248,
+                          //alignment: Alignment.center,
+                          child: Image.file(
+                            File(item.value.split(' space ').first),
+                            fit: BoxFit.contain,
+                            filterQuality: FilterQuality.high,
+                          )),
+                    );
+                  }).toList(),
+                ),
+              ),
+              Container(
+                constraints: const BoxConstraints(minHeight: 400),
+                padding: const EdgeInsets.all(10),
+                //height: MediaQuery.of(context).size.height,
+                width: 550,
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black, width: 2)),
+                child: Wrap(
+                  direction: Axis.horizontal,
+                  spacing: 10,
+                  runSpacing: 10,
+                  children: values.map((item) {
+                    return DragTarget<ItemModel>(
+                      onAccept: (receivedItem) async {
+                        if (item.value ==
+                            receivedItem.value.split(' space ').last) {
+                          setState(() {
+                            // playConfetti = true;
+                            // _smallConfettiController.play();
+                            // _confettiRightController.play();
+                            // _confettiLeftController.play();
+
+                            items.remove(receivedItem);
+                            // widget.items2.remove(item);
+                            //dispose();
+                            // score += 1;
+                            //item = receivedItem;
+                            item.accepting = false;
+                            item.isSuccessful = true;
+                            item.value = receivedItem.value;
+                            //print('....matched......'); //hot reload.....
+                          });
+
+                          //await audioPlay();
+                        } else {
+                          setState(() {
+                            //score -= 1;
+                            // print('*' +
+                            //     item.value +
+                            //     '*aaa*' +
+                            //     receivedItem.value.split(' space ').last +
+                            //     "*");
+                            item.accepting = false;
+                            //playConfetti = false;
+                          });
+                        }
+                      },
+                      onLeave: (receivedItem) {
+                        setState(() {
+                          item.accepting = false;
+                          //playConfetti = false;
+                          //audioPlayer.stop();
+                        });
+                      },
+                      onWillAccept: (receivedItem) {
+                        setState(() {
+                          item.accepting = true;
+                          //playConfetti = false;
+                        });
+                        return true;
+                      },
+                      builder: (context, acceptedItem, rejectedItem) =>
+                          Container(
+                        color: item.accepting
+                            ? Colors.red
+                            : item.isSuccessful
+                                ? Colors.white
+                                : Colors.blue,
+                        height: 168,
+                        width: 248,
+                        alignment: Alignment.center,
+                        //margin: const EdgeInsets.all(8.0),
+                        child: item.isSuccessful
+                            ? Image.file(
+                                File(item.value.split(' space ').first),
+                                fit: BoxFit.contain,
+                                filterQuality: FilterQuality.high,
+                              )
+                            : Text((int.parse(item.value) + 1).toString(),
+                                style: const TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 22.0)),
+                      ),
+                    );
+                  }).toList(),
+                ),
+              )
+            ],
+          )),
     );
   }
 }
